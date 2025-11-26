5d0a6661527426269160ba38038e53c5b9fd4aac
Mauren <khozamauren@gmail.com>
2025-11-26 11:25:48 +0200

    create a sprongboot files

---
+++ Tasklist/src/main/java/com/example/Tasklist/SpringbootTasklistApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTasklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTasklistApplication.class, args);
+	}
+
+}
